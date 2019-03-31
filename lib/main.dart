1c29007f718e3c10d9ec3d8f9cba9f4b02fdfb38
marcelo <marcelo.tk3@gmail.com>
2019-03-30 23:13:47 -0500

    COnfigurando ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'menu.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/scheduler.dart' show timeDilation;
 
 void main(){
   //timeDilation = 3.0;
+  //debugPaintSizeEnabled = false,
   runApp(
     MaterialApp(
-        title: 'Inspiración',
-        theme: ThemeData.light(),
-        home: Menu()
+      debugShowCheckedModeBanner: false,
+      title: 'Inspiración',
+      theme: ThemeData.light(),
+      home: Menu()
     )
   );
 }
